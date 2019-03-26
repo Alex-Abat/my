2ab4da07cb0a3668768f7acbdf0c604459e0c1e1
Alex-Abat <a.abatnin@mail.ru>
2019-03-26 17:09:26 +0800

    hdhdh

--- hello.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def wsgi_application(environ, start_response):
-	body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
-	header = [ ('Content-Type', 'text/plain') ]
-	status = '200 OK'
-	start_response(status, header)
-	return body
--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/ask.py  /etc/gunicorn.d/ask.py
+sudo ln -sf /home/box/web/etc/hello.conf  /etc/gunicorn.d/hello.py
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
-sudo ln -sf /home/box/web/etc/ask.py  /etc/gunicorn.d/test2
