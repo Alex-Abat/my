f0be3457da673c920378ac56fff9e89557da5a9c
Alex-Abat <47635101+Alex-Abat@users.noreply.github.com>
2019-03-26 18:38:58 +0800

    Update urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,9 +1,6 @@
-from qa.views import test
-urlpatterns = patterns('qa.views',
-    url(r'^$', test, name='qa'),
-    url(r'^login/', test, name='login'),
-    url(r'^question/\d\d\d/', test, name='question'),
-    url(r'^ask', test, name='ask'),
-    url(r'^popular', test, name='popular'),
-    url(r'^new/', test, name='new'),
-)
+from django.conf.urls import url
+from views import test
+
+urlpatterns = [
+    url(r'.*', test),
+]
