d6d928712bfb621676b942d3a9c37dd5416ace4c
Alex-Abat <a.abatnin@mail.ru>
2019-03-31 13:05:45 +0800

    11

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/ask.py  /etc/gunicorn.d/ask.py
-sudo ln -sf /home/box/web/etc/hello.conf  /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/gunicorn_django.conf  /etc/gunicorn.d/gunicorn_django.conf 
+sudo ln -sf /home/box/web/etc/gunicorn_hello.conf  /etc/gunicorn.d/gunicorn_hello.conf
 sudo /etc/init.d/nginx restart
