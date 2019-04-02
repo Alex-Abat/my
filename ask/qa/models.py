08384beb57a27d8b8b523d2162efaf226fc27e7a
alex-abat <a.abatnin@mail.ru>
2019-04-02 02:42:47 +0000

    MySql

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,16 +2,26 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+    def popular(self):
+        return self.order_by('-rating')
+
 class Question(models.Model):
-    title = models.CharField(max_lenght=255)
+    title = models.CharField(max_length=255)
     text = models.TextField()
-    added_at = models.DateTimeField()
-    rating = models.IntegerField()
-    author = models.CharField(max_length=255)
-    likes = models.IntegerField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User)
+    likes = models.ManyToManyField(User, related_name='question_like_user')
+    objects = QuestionManager()
 
 class Answer(models.Model):
     text = models.TextField()
-    addes_at = models.DateTimeField()
+    added_at = models.DateTimeField(auto_now_add=True)
     question = models.ForeignKey(Question)
+    author = models.ForeignKey(User)
