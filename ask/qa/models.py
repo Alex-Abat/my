3fa6d99a902367f20f7abbf5861a929132dc5281
Alex-Abat <a.abatnin@mail.ru>
2019-04-02 09:58:12 +0800

    MySql

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -3,3 +3,15 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_lenght=255)
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    rating = models.IntegerField()
+    author = models.CharField(max_length=255)
+    likes = models.IntegerField()
+
+class Answer(models.Model):
+    text = models.TextField()
+    addes_at = models.DateTimeField()
+    question = models.ForeignKey(Question)
